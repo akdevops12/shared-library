ae619c91c0b72ebaad9053226863d02e175cae4b
armand k <akdevops12@gmail.com>
2022-08-23 13:21:45 -0400

    adding compose file

--- vars/sendNotificationSuccess.groovy
+++
@@ -1,7 +0,0 @@
-// Send Slack build successful notification
-def call() {
-  sendNotifications(
-    slackSendColor: "good", //"#00FF00",
-    message: "Hey good job. Code deployed SUCCESSFUL"
-  )
-}
